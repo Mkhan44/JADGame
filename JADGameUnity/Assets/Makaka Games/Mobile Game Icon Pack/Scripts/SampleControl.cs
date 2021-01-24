3c4f52b58c77af3f816a52633fa5352d3ed64836
Mohamed R. Khan <khanantisora@hotmail.com>
2021-01-23 23:02:48 -0500

    Imported packages + got Ad related stuff ready
    Prepping project for full development.

---
+++ JADGameUnity/Assets/Makaka Games/Mobile Game Icon Pack/Scripts/SampleControl.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SampleControl : MonoBehaviour 
+{
+	public Animator gameOverMenuAnimator;
+
+	void Start () 
+	{
+		if(gameOverMenuAnimator)
+		{
+			gameOverMenuAnimator.SetTrigger("Show");
+			//Debug.Log("Game Over Menu Show");
+		}
+	}
+}
