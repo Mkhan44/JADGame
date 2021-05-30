6d203dd00e23f326a841cef261cfea1bbeb5fe36
Mohamed R. Khan <khanantisora@hotmail.com>
2021-05-30 16:22:30 -0400

    Implemented new graphics for menus and gameplay
    New graphics implemented, build for 05-30-2021 ready to go for Android and IOS.

---
+++ JADGameUnity/Assets/Scripts/Gameplay/Tutorial/Tutorial_Instance_Debug.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial_Instance_Debug : MonoBehaviour
+{
+    public static Tutorial_Instance_Debug instance;
+
+    bool isTutorial;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(this);
+    }
+    void Start()
+    {
+        isTutorial = false;
+    }
+
+    public void setTutorial(bool setVal)
+    {
+        isTutorial = setVal;
+    }
+
+    public bool getTutorialVal()
+    {
+        return isTutorial;
+    }
+}
