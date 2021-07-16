d4ccad877b781f4eb81790f21deff46159ed0c37
Mohamed R. Khan <khanantisora@hotmail.com>
2021-07-16 01:15:43 -0400

    Added transition from main menu to gameplay scene.
    Main menu transitions to gameplay scene now somewhat seamlessly.

--- JADGameUnity/Assets/Scripts/Menus/Menu_Manager.cs
+++ JADGameUnity/Assets/Scripts/Menus/Menu_Manager.cs
@@ -6,11 +6,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Menu_Manager : MonoBehaviour
 {
     [Header("Singleton")]
     public static Menu_Manager instance;
+
+    [Header("Fade to gameplay stuff")]
+    [SerializeField] GameObject fadePanel;
+    [SerializeField] Color panelColor;
 
     Scene gameplayScene;
 
@@ -36,7 +41,9 @@ public class Menu_Manager : MonoBehaviour
 
         //Might want to make sure that this string works.
 
-        SceneManager.LoadScene("Gameplay");
+        StartCoroutine(fadeToGameplay());
+
+
     }
 
     public void tutorialToggle(bool toggleVal)
@@ -45,6 +52,41 @@ public class Menu_Manager : MonoBehaviour
         {
             Tutorial_Instance_Debug.instance.setTutorial(toggleVal);
         }
+    }
+    
+    IEnumerator fadeToGameplay()
+    {
+
+        RectTransform fadePanelTransform = fadePanel.GetComponent<RectTransform>();
+        Vector3 currentScale = fadePanelTransform.localScale;
+        Vector3 increaseScaleRate = new Vector3(1, 1, 1);
+
+        float i = 0.0f;
+        float rate = 0.0f;
+        Color32 startColor = new Color32(255, 255, 255, 10);
+        Color32 endColor = new Color32(255, 255, 255, 255);
+        Image fadePanelImg = fadePanel.GetComponent<Image>();
+
+        rate = (1.0f / 2.5f) * 1.0f;
+   
+
+        while (i < 1.0f)
+        {
+            currentScale += increaseScaleRate;
+            fadePanelTransform.localScale = currentScale;
+            i += Time.deltaTime * rate;
+
+            fadePanelTransform.localScale = Vector3.Lerp(fadePanelTransform.localScale, new Vector3(20, 20, 20), (i));
+
+            fadePanelImg.color = Color32.Lerp(startColor, endColor, (i));
+            yield return null;
+
+          //  yield return new WaitForSeconds(0.001f);
+        }
+
+        Debug.Log("Loading the gameplay scene...");
+        SceneManager.LoadSceneAsync("Gameplay");
+
     }
 
 
