17875a753f0bc0fa225a2308719d18a0fe0c6e4f
Mohamed R. Khan <khanantisora@hotmail.com>
2021-10-07 19:05:22 -0400

    Fixed typo in credits, added daily rewards
    Daily rewards feature added to the game + minor bug fixes.

---
+++ JADGameUnity/Assets/Scripts/Daily_Rewards/DailyReward.cs
@@ -0,0 +1,426 @@
+﻿using System;
+
+using System.Collections;
+
+using System.Collections.Generic;
+
+using UnityEngine;
+
+using UnityEngine.UI;
+
+using TMPro;
+
+using Random = UnityEngine.Random;
+
+
+public class DailyReward : MonoBehaviour {
+
+	//UI
+
+	public TextMeshProUGUI timeLabel; //only use if your timer uses a label
+
+	public Button timerButton; //used to disable button when needed
+
+	public Image _progress;
+
+    public TextMeshProUGUI timeLeft;
+
+    public TextMeshProUGUI timeLeftMenu;
+
+	//TIME ELEMENTS
+
+	public int hours; //to set the hours
+
+	public int minutes; //to set the minutes
+
+	public int seconds; //to set the seconds
+
+	private bool _timerComplete = false;
+
+    private bool _timerIsReady;
+
+	private TimeSpan _startTime;
+
+	private TimeSpan _endTime;
+
+	private TimeSpan _remainingTime;
+
+	//progress filler
+
+	private float _value = 1f;
+
+	//reward to claim
+
+	public int RewardToEarn;
+
+
+
+    //MY VARIABLES
+
+    string numHintsKey = "remainingHints";
+    int numHintsLeft;
+
+    string theTimeLeftStr;
+    public Button closeButton;
+    public TextMeshProUGUI rewardsNumTextMesh;
+    public Button rewardMenuButton;
+
+    //Format:
+
+     TimeSpan timeLeftFormat;
+     float randomFloat;
+
+	//startup
+
+	void Start()
+
+	{
+
+		if (PlayerPrefs.GetString ("_timer") == "")
+
+		{ 
+
+			Debug.Log ("==> Enableing button");
+
+			enableButton ();
+
+		} else 
+
+		{
+
+			disableButton ();
+
+			StartCoroutine ("CheckTime");
+
+		}
+
+	}
+
+
+
+
+
+
+
+	//update the time information with what we got some the internet
+
+	private void updateTime()
+
+	{
+
+		if (PlayerPrefs.GetString ("_timer") == "Standby") {
+
+            PlayerPrefs.SetInt ("_date", TimeManager.sharedInstance.getCurrentDateNow());
+
+            PlayerPrefs.SetString("_timer", TimeManager.sharedInstance.getCurrentTimeNow());
+
+        }else if (PlayerPrefs.GetString ("_timer") != "" && PlayerPrefs.GetString ("_timer") != "Standby")
+
+        {
+
+            int _old = PlayerPrefs.GetInt("_date");
+
+            int _now = TimeManager.sharedInstance.getCurrentDateNow();
+
+          
+            
+
+            //check if a day as passed
+
+            if(_now > _old)
+
+            {//day as passed
+
+                Debug.Log("Day has passed");
+
+                enableButton ();
+
+                return;
+
+            }else if (_now == _old)
+
+            {//same day
+
+                Debug.Log("Same Day - configuring now");
+
+                _configTimerSettings();
+
+                return;
+
+            }else
+
+            {
+
+                Debug.Log("error with date");
+                timeLeftMenu.text = "Can't connect to server!";
+                rewardMenuButton.interactable = false;
+                return;
+
+            }
+
+        }
+
+         Debug.Log("Day had passed - configuring now");
+
+         _configTimerSettings();
+
+	}
+
+
+
+//setting up and configureing the values
+
+//update the time information with what we got some the internet
+
+private void _configTimerSettings()
+
+{
+
+    _startTime = TimeSpan.Parse (PlayerPrefs.GetString ("_timer"));
+
+    _endTime = TimeSpan.Parse (hours + ":" + minutes + ":" + seconds);
+
+    TimeSpan temp = TimeSpan.Parse (TimeManager.sharedInstance.getCurrentTimeNow ());
+
+    TimeSpan diff = temp.Subtract (_startTime);
+
+    _remainingTime = _endTime.Subtract (diff);
+
+    //start timmer where we left off
+
+    setProgressWhereWeLeftOff ();
+
+    
+
+    if(diff >= _endTime)
+
+    {
+
+        _timerComplete = true;
+
+        enableButton ();
+
+    }else
+
+    {
+
+        _timerComplete = false;
+
+        disableButton();
+
+        _timerIsReady = true;
+
+    }
+
+}
+
+
+
+//initializing the value of the timer
+
+	private void setProgressWhereWeLeftOff()
+
+	{
+
+		float ah = 1f / (float)_endTime.TotalSeconds;
+
+		float bh = 1f / (float)_remainingTime.TotalSeconds;
+
+		_value = ah / bh;
+
+        timeLeftFormat = _remainingTime;
+
+		//_progress.fillAmount = _value;
+
+	}
+
+
+
+
+
+
+
+	//enable button function
+
+	private void enableButton()
+
+	{
+        timeLeft.text = "Click below to claim your reward!";
+        timeLeftMenu.text = "Reward Ready!";
+
+        closeButton.interactable = false;
+
+        timerButton.interactable = true;
+
+        rewardMenuButton.interactable = true;
+
+	}
+
+
+
+
+
+
+
+	//disable button function
+
+	private void disableButton()
+
+	{
+        rewardMenuButton.interactable = false;
+        
+
+		timerButton.interactable = false;
+
+        closeButton.interactable = true;
+		//timeLabel.text = "Close";
+
+	}
+
+
+
+
+
+	//use to check the current time before completely any task. use this to validate
+
+	private IEnumerator CheckTime()
+
+	{
+
+		disableButton ();
+
+       // timeLabel.text = "Checking the time...";
+
+        timeLeft.text = "Checking the time...";
+        timeLeftMenu.text = "Checking the time...";
+
+		Debug.Log ("==> Checking for new time");
+
+		yield return StartCoroutine (
+
+			TimeManager.sharedInstance.getTime()
+
+
+
+		);
+
+		updateTime ();
+
+		Debug.Log ("==> Time check complete!");
+
+
+
+	}
+
+
+
+
+
+	//trggered on button click
+
+	public void rewardClicked()
+
+	{
+
+		Debug.Log ("==> Claim Button Clicked");
+
+        claimReward (RewardToEarn);
+
+		PlayerPrefs.SetString ("_timer", "Standby");
+
+		StartCoroutine ("CheckTime");
+
+	}
+
+
+
+
+
+
+
+	//update method to make the progress tick
+
+	void Update()
+
+	{
+        
+
+        if(_timerIsReady)
+
+        {
+
+            if (!_timerComplete && PlayerPrefs.GetString ("_timer") != "")
+
+                {
+                    _value -= Time.deltaTime * 1f / (float)_endTime.TotalSeconds;
+
+                    double newVal = ((double)_value * 100);
+                   // _value -= Time.deltaTime * 1f;
+
+                  
+                    timeLeftMenu.text = theTimeLeftStr + "Already claimed today!";
+
+                //    _progress.fillAmount = _value;
+
+                
+
+                    //this is called once only
+
+                    if (_value <= 0 && !_timerComplete) {
+
+                        //when the timer hits 0, let do a quick validation to make sure no speed hacks.
+
+                    validateTime ();
+
+                    _timerComplete = true;
+
+                }
+
+            }
+
+        }
+
+	}
+
+
+
+
+
+
+
+	//validator
+
+	private void validateTime()
+
+	{
+
+		Debug.Log ("==> Validating time to make sure no speed hack!");
+
+		StartCoroutine ("CheckTime");
+
+	}
+
+
+
+
+
+	private void claimReward(int x)
+
+	{
+        numHintsLeft = PlayerPrefs.GetInt(numHintsKey);
+        //Do randomization with x
+        //between 3 and x-1 ...In the current case x = 11 so between 3-10.
+        int randomNum = Random.Range(3, x);
+
+        PlayerPrefs.SetInt(numHintsKey, (numHintsLeft + randomNum));
+
+		Debug.Log ("You got: "+ randomNum +" Hints!");
+        rewardsNumTextMesh.text = "Congratulations! You just got x" + randomNum + " hints!"; 
+
+
+	}
+
+
+
+}
+
---
+++ JADGameUnity/Assets/Scripts/Daily_Rewards/DayChecker.cs
@@ -0,0 +1,175 @@
+﻿//Code written by Mohamed Riaz Khan of Bukugames.
+//All code is written by me (Above name) unless otherwise stated via comments below.
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Random = UnityEngine.Random;
+public class DayChecker : MonoBehaviour
+{
+    public Button menuButton;
+    public TextMeshProUGUI menuText;
+    public TextMeshProUGUI dialougeText;
+
+    int currentCoins;
+
+    string dateKey = "LastClaimDateKey";
+
+    string dateStored;
+    DateTime oldDate;
+    DateTime newDate;
+    TimeSpan difference;
+
+    bool firstTime = false;
+
+    [Header("Vars we can change")]
+
+    [Tooltip("The minimum reward the user can obtain when claiming.")]
+    public int rewardVarianceMin;
+    [Tooltip("The maximum reward the user can obtain when claiming.")]
+    public int rewardVarianceMax; //Remember to make it this + 1 when randomizing...
+
+    [Tooltip("Hours between rewards. (Must be less than 24)")]
+    public int hoursToWait;
+
+    //Converting this into a percentage so it's not a full 24 hours to wait.
+    float percentageToWait;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (hoursToWait > 24)
+        {
+            hoursToWait = 24;
+        }
+        else if(hoursToWait < 1)
+        {
+            hoursToWait = 1;
+        }
+
+        percentageToWait = (float)hoursToWait / 24f;
+
+ 
+
+
+        newDate = System.DateTime.Now;
+        if(PlayerPrefs.GetString(dateKey) == "")
+        {
+            string settingDate = Convert.ToString(newDate);
+            Debug.Log("There is no data: Setting the date to - " + settingDate);
+            PlayerPrefs.SetString(dateKey, settingDate);
+            firstTime = true;
+        }
+        else
+        {
+            firstTime = false;
+        }
+
+        dateStored = PlayerPrefs.GetString(dateKey);
+        oldDate = Convert.ToDateTime(dateStored);
+
+//        Debug.Log("Last date: " + oldDate);
+  //      Debug.Log("Current date: " + newDate);
+
+
+        
+    }
+    void Start()
+    {
+        currentCoins = Collect_Manager.instance.totalCoins;
+
+        //Debug.Log(PlayerPrefs.GetString(dateKey));
+        canWeClaim();
+       
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      // canWeClaim();
+    }
+
+    bool canWeClaim()
+    {
+        newDate = System.DateTime.Now;
+
+        //Checks if the current date is at least 1 higher than previous one we stored.
+        TimeSpan difference = newDate.Subtract(oldDate);
+
+       // Debug.Log("The difference is: " + difference.TotalDays);
+       // Debug.Log("Percentage to wait is: " + percentageToWait + " Which is hrs: " + hoursToWait);
+        if(firstTime)
+        {
+            menuText.text = "Daily reward!";
+            menuButton.interactable = true;
+            PlayerPrefs.SetString(dateKey, "");
+            return true;
+        }
+        else
+        {
+       
+            if (difference.TotalDays >=  percentageToWait)
+            {
+                menuText.text = "Daily reward!";
+                menuButton.interactable = true;
+                return true;
+            }
+            else
+            {
+                menuText.text = "Claimed already";
+                menuButton.interactable = false;
+                return false;
+            }
+        }
+       
+    }
+
+
+
+    public void claimReward()
+    {
+
+        //Give player random num of hints between the variance.
+        int coinsToGive;
+
+        coinsToGive = Random.Range(rewardVarianceMin , (rewardVarianceMax+1));
+
+        float roundedNum = Mathf.Round(coinsToGive / 100);
+
+        Debug.Log("Rounded coins are: " + roundedNum);
+
+        coinsToGive = (int)roundedNum * 100;
+
+        //Debug.Log("coins we are giving to the player is: " + coinsToGive);
+
+        dialougeText.text = "You just got x" + coinsToGive + " Coins! Come back tomorrow for more rewards!";
+
+        currentCoins += coinsToGive;
+
+        Collect_Manager.instance.totalCoins = currentCoins;
+
+        Save_System.SaveCollectables(Collect_Manager.instance);
+
+        firstTime = false;
+
+        onRewardClaimed();
+
+    }
+
+    void onRewardClaimed()
+    {
+        //Makes it so the current date we are on is now the "previous date" for check.
+
+        string newStringDate = Convert.ToString(newDate);
+
+        PlayerPrefs.SetString(dateKey, newStringDate);
+
+        oldDate = Convert.ToDateTime(newStringDate);
+        //Call to make sure that the messages reset.
+        canWeClaim();
+    }
+}
